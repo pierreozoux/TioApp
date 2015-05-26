8f5fa6fd50c12dc28e9f37129718337821380a0c
pierreozoux <pierre@ozoux.net>
2015-05-26 18:12:10 +0200

    Adds import feedback - fixes #4

--- import-integradas/client/index.js
+++ import-integradas/client/index.js
@@ -1,2 +1,36 @@
 Router.route('import');
 
+Uploader.status = function(error, data, textStatus, jqXHR) {
+  if(error) {
+    Session.set('errorResult', error.reason);
+    console.log(error + ' ' + data + ' ' + textStatus + ' ' + jqXHR);
+    $('#errorResult').collapse('show');
+  } else {
+    $('#errorResult').collapse('hide');
+    $('#successResult').collapse('hide');
+  }
+};
+
+Uploader.finished = function(index, fileInfo) {
+  console.log('Finished upload, starting csv import.');
+  $('#processing').collapse('show');
+  Meteor.call('importIntegradas', fileInfo.path, function (error) {
+    window.setTimeout(function() {
+      $('#processing').collapse('hide');
+    }, 1000);
+    
+    if (error) {
+      Uploader.status(error);
+    } else {
+      $('#successResult').collapse('show');
+    }
+    console.log('Finished processing CSV.');
+  });
+};
+
+Template.import.helpers({
+  errorResult: function() {
+    return Session.get('errorResult');
+  }
+})
+
