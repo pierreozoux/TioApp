c07f0a908a66f7edb89e8a1ee400a3171312897d
pierreozoux <pierre@ozoux.net>
2015-07-17 10:35:00 +0100

    better error message

--- collections/helpers.js
+++ collections/helpers.js
@@ -1,7 +1,7 @@
 SimpleSchema.messages({
   uniqueEmail: '[label] must be unique',
   regEx: [
-    {exp: /^[a-zA-Z0-9- ]+$/, msg: '[label] must not contain other chars than: 0-9, a-z, A-Z, "-" and " "'},
+    {exp: /^[a-zA-Z0-9- ]+$/, msg: '[label] - "[value]" must not contain other chars than: 0-9, a-z, A-Z, "-" and " "'},
     {exp: /^[0-9]{9}$/, msg: '[label] must be 9 digits, without any other chars.'}
   ]
 });
