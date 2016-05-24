278f51011d5ff833f7d95b89947f581dc33333d3
pierreozoux <pierre@ozoux.net>
2016-05-24 19:51:44 +0200

    closes #50

---
+++ server/router.js
@@ -0,0 +1,33 @@
+Router.route('/contacts/download', function() {
+  var user = '';
+  log.info(this.request.cookies.meteor_login_token);
+  if(this.request.cookies.meteor_login_token)
+      user = Meteor.users.findOne({"services.resume.loginTokens.hashedToken": Accounts._hashLoginToken(this.request.cookies.meteor_login_token)});
+  if (user && Houston._user_is_admin(user._id)) {
+    log.info(user);
+    var data = [];
+    var fileData = "";
+    var stateSupport;
+    var contact;
+    Orders.find().forEach(function(order) {
+      contact = order.getContact();
+      if (contact) {
+        stateSupport = contact.stateSupport.toString();
+      } else {
+        stateSupport = '';
+      }
+      stateSupport = 
+        data.push({
+        name: order.name,
+        phone: order.phone,
+        course: order.courseName,
+        stateSupport: stateSupport
+      });
+    });
+    this.response.writeHead(200, {
+      'Content-type': 'text/csv',
+      'Content-Disposition': "attachment; filename=contacts.csv"
+    });
+    this.response.end(CSV.unparse(data));
+  }
+}, {where: 'server'});
