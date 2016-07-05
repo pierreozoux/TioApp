65d5282d579800b7e6500796b9dd79a45257ba61
pierreozoux <pierre@ozoux.net>
2016-07-05 12:06:42 +0100

    archive old resources

---
+++ .hotfix/20160704-archive-old-resources.js
@@ -0,0 +1,5 @@
+db.schools.renameCollection('schools2015');
+db.courses.renameCollection('courses2015');
+db.groupOrderedResources.renameCollection('groupOrderedResources2015');
+db.groupOrders.renameCollection('groupOrders2015');
+db.resources.renameCollection('resources2015');
