4d2a865c12cb3417fea5ecfc3a3d5563a424fbec
pierreozoux <pierre@ozoux.net>
2016-07-08 15:02:38 +0100

    Send only to mobile!

--- server/lib/sms.js
+++ server/lib/sms.js
@@ -30,28 +30,33 @@ Meteor.startup(function () {
       pollInterval: 1000
     },
     function (job, callback) {
-      // Create a client:
-      var client = new twilio.RestClient(Settings.findOne({key: 'SMS-ACCOUNT-SID'}).value, Settings.findOne({key: 'SMS-AUTH-TOKEN'}).value );
-      console.log('myTwilio.RestClient initialized');
-      var to = '+351' + job.data.phone;
+      if (job.data.phone.substring(0,1) != '2') {
+        // Create a client:
+        var client = new twilio.RestClient(Settings.findOne({key: 'SMS-ACCOUNT-SID'}).value, Settings.findOne({key: 'SMS-AUTH-TOKEN'}).value );
+        console.log('myTwilio.RestClient initialized');
+        var to = '+351' + job.data.phone;
 
-      client.sendMessage({
-        to: to, // Any number Twilio can deliver to
-        from: Settings.findOne({key: 'sms-from'}).value, // +15005550006 : A number you bought from Twilio and can use for outbound communication
-        body: job.data.text // body of the SMS message
-      }, Meteor.bindEnvironment((err,res) => {
-        if (!err) {
-          console.log('myTwilio.RestClient resp from : ' + res.from); // outputs "+14506667788"
-          console.log('myTwilio.RestClient resp body: ' + res.body); // outputs "word to your mother."
-          job.done('sms sent!');
-          job.remove();
-        } else {
-          pretty_error = JSON.stringify(err, null, 2)
-          console.log('err: ' + pretty_error);
-          job.fail(pretty_error);
-        }
-        callback()
-      }));
+        client.sendMessage({
+          to: to, // Any number Twilio can deliver to
+          from: Settings.findOne({key: 'sms-from'}).value, // +15005550006 : A number you bought from Twilio and can use for outbound communication
+          body: job.data.text // body of the SMS message
+        }, Meteor.bindEnvironment((err,res) => {
+          if (!err) {
+            console.log('myTwilio.RestClient resp from : ' + res.from); // outputs "+14506667788"
+            console.log('myTwilio.RestClient resp body: ' + res.body); // outputs "word to your mother."
+            job.done('sms sent!');
+            job.remove();
+          } else {
+            pretty_error = JSON.stringify(err, null, 2)
+            console.log('err: ' + pretty_error);
+            job.fail(pretty_error);
+          }
+          callback()
+        }));
+      } else {
+        job.done('not a mobile!');
+        job.remove();
+      }
     }
   );
 });
@@ -60,16 +65,18 @@ Meteor.methods({
   sendSMSAll: function (text) {
     check([text], [String]);
     _.each(phonesToContact(), function(phone) {
-      var job = new Job(smsJobs, 'sms', {
-        phone: phone,
-        text: text
-      });
+      if (job.data.phone.substring(0,1) != '2') {
+        var job = new Job(smsJobs, 'sms', {
+          phone: phone,
+          text: text
+        });
 
-      job.priority('normal')
-      .retry({
-        retries: 5,
-        wait: 60
-      }).save();
+        job.priority('normal')
+        .retry({
+          retries: 5,
+          wait: 60
+        }).save();
+      }
     });
   },
   priceSMSs: function() {
