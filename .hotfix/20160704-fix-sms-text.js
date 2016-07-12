817d82eb7b45af123d55ec231f0ff6b2e58848a3
pierreozoux <pierre@ozoux.net>
2016-07-12 12:01:42 +0100

    fix expensive sms.

---
+++ .hotfix/20160704-fix-sms-text.js
@@ -0,0 +1,1 @@
+db.smsJobQueue.jobs.update({}, { $set: { 'data.text': 'Faca a sua encomenda de livros escolares no TIO PAPEL, até ao fim do mes, para ter 10% de Desconto. Utilize o tel. 210739830 ou o mail barreiro@tiopapel.com' } }, {multi: true});
